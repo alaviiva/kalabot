e45b01924c6b1d0bb34fe2a61de2e683f4eee8a9
Sampo Vartiala <sampo.vartiala@gmail.com>
2020-02-09 18:34:21 +0200

    Test rolling

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 
 require('dotenv').config();
 
+const chance = require('chance').Chance();
 const TelegramBot = require('node-telegram-bot-api');
 const token = process.env.TG_API_TOKEN;
 const bot = new TelegramBot(token, {polling: true});
-console.log("start");
-
-bot.onText(/\/start/, (msg) => {
-  bot.sendMessage(msg.chat.id, "Kala");
-});
 
 bot.onText(/\/kala/, (msg) => {
-  bot.sendMessage(msg.chat.id, "Olet kala");
+  bot.sendMessage(msg.chat.id, 'Olet kala');
+});
+
+bot.onText(/\/roll (.+)/, (msg, match) => {
+  console.log(match[1]);
+  const result = chance.rpg(match[1], {sum: true});
+  bot.sendMessage(msg.chat.id, result);
 });
